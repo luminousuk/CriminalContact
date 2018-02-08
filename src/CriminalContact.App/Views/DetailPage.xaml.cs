1ef0e197f24f1efc474b3ba8aeafba41336d46dd
Chris Pickford <chris@luminousuk.com>
2018-02-08 11:20:30 +0000

    Added hamburger UWP project

---
+++ src/CriminalContact.App/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CriminalContact.App.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace CriminalContact.App.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
