61fde3d7f627f78d84700d942e8abc22169c1d85
Chris Pickford <chris@luminousuk.com>
2019-02-01 14:46:22 +0000

    Replaced material icons with iconic. Templatised sidebar.

--- src/renderer/app/sidebar/sidebar.component.ts
+++ src/renderer/app/sidebar/sidebar.component.ts
@@ -1,11 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 
+interface ISidebarItem {
+  iconClass: string;
+  title: string;
+  href: string;
+  floatBottom: boolean;
+}
+
 @Component({
   selector: 'cc-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+
+  items: ISidebarItem[] = [
+    {
+      iconClass: "home",
+      title: "Home",
+      href: "#",
+      floatBottom: false
+    },
+    {
+      iconClass: "bar-chart",
+      title: "Dashboard",
+      href: "#",
+      floatBottom: false
+    },
+    {
+      iconClass: "people",
+      title: "Players",
+      href: "#",
+      floatBottom: false
+    },
+    {
+      iconClass: "credit-card",
+      title: "Banking",
+      href: "#",
+      floatBottom: false
+    },
+    {
+      iconClass: "cog",
+      title: "Settings",
+      href: "#",
+      floatBottom: true
+    }
+  ];
 
   constructor() { }
 
