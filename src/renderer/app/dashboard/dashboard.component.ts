107d652e538d85e0a6ab504176220f032244a90f
Chris Pickford <chris@luminousuk.com>
2019-02-01 15:23:22 +0000

    Added placeholder dashboard component

---
+++ src/renderer/app/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cc-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
