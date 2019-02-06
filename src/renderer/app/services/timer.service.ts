816e11e2da0133b6ac94e893806258da4be914dc
Chris Pickford <chris@luminousuk.com>
2019-02-06 21:44:13 +0000

    Refactored TimerService

--- src/renderer/app/services/timer.service.ts
+++ src/renderer/app/services/timer.service.ts
@@ -1,66 +1,31 @@
 import { Injectable } from '@angular/core';
-import { interval } from "rxjs";
+import { interval, Subscription } from "rxjs";
 import { Guid } from "guid-typescript";
-
-class TimerAction {
-  private _lastRun: Date;
-  private _id: Guid;
-  constructor(
-    private _action: () => void,
-    private _interval: number
-  ) {
-    this._id = Guid.create();
-    this._lastRun = new Date(0);
-  }
-
-  public get id(): Guid {
-    return this._id;
-  }
-
-  public get shouldRun(): boolean {
-    return Date.now() > this._lastRun.getTime() + this._interval;
-  }
-
-  public run(): void {
-    this._action();
-    this._lastRun = new Date();
-  }
-
-}
 
 @Injectable({
   providedIn: 'root'
 })
 export class TimerService {
-
-  private _ticker = interval(500);
-
-  private _actions: TimerAction[] = [];
+  private _subscriptions: Map<string, Subscription>;
 
   constructor() {
-    this._ticker.subscribe(() => this.performActions());
+    this._subscriptions = new Map<string, Subscription>();
   }
 
-  private performActions(): void {
-    if (this._actions.length === 0) {
-      return;
-    }
+  public subscribe(action: () => void, intervalMs: number): string {
+    const guid = Guid.create().toString();
+    const subscription = interval(intervalMs).subscribe(action);
+    this._subscriptions.set(guid, subscription);
 
-    for (let action of this._actions) {
-      if (action.shouldRun) {
-        action.run();
-      }
-    }
-  }
-
-  public subscribe(action: () => void, interval: number): string {
-    const timerAction = new TimerAction(action, interval);
-    this._actions.push(timerAction);
-
-    return timerAction.id.toString();
+    return guid;
   }
 
   public unsubscribe(id: string): void {
-    this._actions = this._actions.filter(a => a.id.toString() !== id);
+    if (!this._subscriptions.has(id)) {
+      throw new Error(`Subscription with id ${id} does not exist.`);
+    }
+
+    this._subscriptions.get(id).unsubscribe();
+    this._subscriptions.delete(id);
   }
 }
