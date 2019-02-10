b145c78ae832711772992b52978062c9360460bb
Chris Pickford <chris@luminousuk.com>
2019-02-10 16:14:59 +0000

    Replaced SelectPlayerComponent with ng-select / Added CurrencyMaskModule

--- src/renderer/app/app.module.ts
+++ src/renderer/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { NgSelectModule } from "@ng-select/ng-select";
+import { CurrencyMaskModule } from "ng2-currency-mask";
 
 import { ElapsedTimePipe } from "./pipes/elapsed-time.pipe";
 
@@ -11,8 +13,6 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { BankingComponent } from './components/banking/banking.component';
 import { PlayersComponent } from './components/players/players.component';
-import { SelectPlayerComponent } from './components/select-player/select-player.component';
-import { SelectRoleComponent } from './components/select-role/select-role.component';
 
 @NgModule({
   declarations: [
@@ -21,15 +21,15 @@ import { SelectRoleComponent } from './components/select-role/select-role.compon
     SidebarComponent,
     DashboardComponent,
     BankingComponent,
-    PlayersComponent,
-    SelectPlayerComponent,
-    SelectRoleComponent
+    PlayersComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    NgbModule
+    NgbModule,
+    NgSelectModule,
+    CurrencyMaskModule
   ],
   providers: [],
   bootstrap: [AppComponent]
