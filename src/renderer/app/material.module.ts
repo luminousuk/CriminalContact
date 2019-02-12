b3f8ebac013d6a648ee7823fffc745ca0c562c22
Chris Pickford <chris@luminousuk.com>
2019-02-12 22:16:10 +0000

    no idea

--- src/renderer/app/material.module.ts
+++ src/renderer/app/material.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import {
     MatButtonModule,
     MatDialogModule,
     MatIconModule,
+    MatSidenavModule,
     MatTooltipModule
 } from "@angular/material";
 
 @NgModule({
     imports: [
+        FlexLayoutModule,
         MatButtonModule,
         MatDialogModule,
         MatIconModule,
+        MatSidenavModule,
         MatTooltipModule
     ],
     exports: [
+        FlexLayoutModule,
         MatButtonModule,
         MatDialogModule,
         MatIconModule,
+        MatSidenavModule,
         MatTooltipModule
     ]
 })
