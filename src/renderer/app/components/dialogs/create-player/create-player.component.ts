805e7ee265c410dd4075cc098a2898fc9ae9a5f4
Chris Pickford <chris@luminousuk.com>
2019-02-12 15:46:02 +0000

    Updated CreatePlayerComponent

--- src/renderer/app/components/dialogs/create-player/create-player.component.ts
+++ src/renderer/app/components/dialogs/create-player/create-player.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import IPlayerRole from 'src/renderer/app/models/roles/playerrole.i';
 import { RoleService } from 'src/renderer/app/services/role.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialogRef } from '@angular/material';
 
 @Component({
   templateUrl: './create-player.component.html'
 })
-export class CreatePlayerComponent {
+export class CreatePlayerComponent implements OnInit {
 
   constructor(
-    private readonly _roleService: RoleService
+    private readonly _roleService: RoleService,
+    private readonly _dialogRef: MatDialogRef<CreatePlayerComponent>
   ) { }
+
+  ngOnInit() {
+  }
 
   public firstName: string;
   public lastName: string;
@@ -18,5 +24,9 @@ export class CreatePlayerComponent {
 
   public get roles(): IPlayerRole[] {
     return this._roleService.availableRoles;
+  }
+
+  public save(form): void {
+    
   }
 }
